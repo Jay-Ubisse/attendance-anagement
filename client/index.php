03f63055dcc58dbc59d6c9eb5037144c6fc5379f
Jay-Ubisse <joaquimubisse@gmail.com>
2023-01-13 17:15:51 +0200

    adding the login page

--- client/index.php
+++ client/index.php
@@ -7,8 +7,21 @@
     <title>Inicio | Gestão de Presenças</title>
     <link rel="stylesheet" href="./assets/styles/output.css">
     <link rel="stylesheet" href="./assets/styles/form.css">
+    <script src="https://kit.fontawesome.com/4b43862993.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <div class="container">
+		<div class="pic">
+			<i class="fas fa-user"></i>
+		</div>
+		<h2 class="title">Login</h2>
+		<form class="form" method="POST">
+			<label for="username">Nome de Usuário</label>
+			<input type="text" id="username" class="input" autocomplete="off" placeholder="Insira o nome de usuário" />
+			<label for="password">Palavra-passe</label>
+			<input type="text" id="password" class="input" autocomplete="off" placeholder="Insira a palavra-passe" />
+			<button type="submit" class="button">Entrar</button>
+		</form>
+	</div>
 </body>
 </html>
